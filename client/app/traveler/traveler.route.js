5faa2a6dd492fec2e44a2e995ba9815e1eb25c0b
mbusso <mbusso@hexacta.com>
2015-05-08 20:23:01 +0000

    add smart table module

--- client/app/traveler/traveler.route.js
+++ client/app/traveler/traveler.route.js
@@ -7,5 +7,10 @@ angular.module('contentBuilderApp')
         url: '/traveler/new',
         templateUrl: 'app/traveler/traveler-new.html',
         controller: 'TravelerCtrl'
+      })
+      .state('traveler-list', {
+        url: '/traveler/list',
+        templateUrl: 'app/traveler/list/traveler-list.html',
+        controller: 'TravelerListCtrl'
       });
   });
