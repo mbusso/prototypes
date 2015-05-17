47e0bcd3d8d681e39766502f608e0b3d63de9195
mbusso <matiassbb@gmail.com>
2015-05-17 19:44:00 +0000

    redirect to main page after new or edit entity

--- client/app/traveler/traveler-edit.controller.js
+++ client/app/traveler/traveler-edit.controller.js
@@ -1,13 +1,14 @@
 'use strict';
 
 angular.module('contentBuilderApp')
-.controller('TravelerEditCtrl', function ($scope, Traveler, travelerToUpdate) {
+.controller('TravelerEditCtrl', function ($scope, $state, Traveler, travelerToUpdate) {
 
 	$scope.traveler = travelerToUpdate;
 
 	$scope.save = function() {		
 		Traveler.update($scope.traveler).$promise.then(function() {
 			$scope.feedback = "traveler updated";
+			$state.go('traveler-list');
 		}, function(error){
 			$scope.feedback = error;
 		});
--- client/app/traveler/traveler.controller.js
+++ client/app/traveler/traveler.controller.js
@@ -1,12 +1,13 @@
 'use strict';
 
 angular.module('contentBuilderApp')
-.controller('TravelerCtrl', function ($scope, Traveler) {
+.controller('TravelerCtrl', function ($scope, $state, Traveler) {
   $scope.traveler = {};
 
   $scope.save = function() {
     Traveler.save($scope.traveler).$promise.then(function() {
       $scope.feedback = "traveler saved";
+      $state.go('traveler-list');
     }, function(error){
       $scope.feedback = error;
     });
